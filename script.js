4941b736b85d283982cd3261575383ed4d59b2c3
MohammedAnis <mohammedanismultani@gmail.com>
2024-05-25 23:46:26 +0530

    rehmani tools edited

--- script.js
+++ script.js
@@ -4,34 +4,38 @@ const productArray = [
     img_src: "images/miniroundnoseplier.JPG",
     name: "REHMANI Mini Round Nose Pliers",
     details: "Essential Tool for Jewelry Making and Fine Detail Work - Precision Crafting",
-    grip: "Dual Colour Grip",
+    price: "₹149",
+    grip: "Black & Orange Grip",
     item: "AFT 001",
     size: "8*",
   },
   {
     title: "",
-    img_src: "images/plier.JPG",
+    img_src: "images/mininoseplier.JPG",
     name: "REHMANI Mini Nose Pliers",
     details: "Precision Craftsmanship in a Compact Design for Jewelry Making and Electronics Repair",
-    grip: "Orange Colour Grip",
+    price: "149",
+    grip: "Black & Orange Grip",
     item: "AFT 002",
     size: "8*",
   },
   {
     title: "",
-    img_src: "images/plier.JPG",
+    img_src: "images/punchingplier.JPG",
     name: "REHMANI Heavy Duty Punching Pliers",
     details: "Professional Leathercraft Tool for Precise Hole Punching and Grommet Setting for jewelry making use",
-    grip: "GT Grip",
+    price: "₹199",
+    grip: "Black & Orange Grip",
     item: "AFT 003",
     size: "8*",
   },
   {
-    title: "AFT 555",
-    img_src: "images/plier.JPG",
-    name: "COMBINATION PLIER",
-    details: "Induction Hardnened",
-    grip: "Blue HV Grip",
+    title: "",
+    img_src: "images/jewelrymakingcutter.JPG",
+    name: "REHMANI Precision JC-18 Jewelry Making Cutter",
+    details: "Essential Tool for Crafting Exquisite Designs",
+    price: "₹199",
+    grip: "Orange Grip",
     item: "AFT 004",
     size: "8*",
   },
@@ -68,6 +72,7 @@ productArray.map((value) => {
    <img src=${value.img_src} alt="not found"/>
     <h6>${value.name}</h6>
     <p><b>${value.details}</b><p>
+    <p><b>${value.price}</b><p>
     <p>${value.grip}</p>
     <p>Item: ${value.item}</p>
     <p>Size: ${value.size}</p>
